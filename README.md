002a1e8556d29968be4bb4ef5c90fc4fb55041af
nitpum <nitpum@outlook.com>
2018-06-05 16:14:14 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spigot Plugin Template
+Bukkit / Spigot plugin Project template based on Maven
+
+Build
+```bash
+mvn clean install
+```
+
+# License
+MIT [View license](license.txt)
---
+++ src/main/java/com/nitpum/mcplugin/spigotplugin/Main.java
@@ -0,0 +1,18 @@
+package com.nitpum.mcplugin.spigotplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+  
+  
+  @Override
+  public void onEnable () {
+    // On plugin enable
+  }
+
+  @Override
+  public void onDisable () {
+    // On plugin disable
+  }
+
+}
